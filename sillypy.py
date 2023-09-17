52d386fa1b72741550dbcc4d92c5e99d9d652fda
VADIM TSARFIN <vtsarfin@gmail.com>
2023-09-17 09:19:03 +0300

    Initial commit.

---
+++ sillypy.py
@@ -0,0 +1,38 @@
+import configparser
+import getopt
+import sys
+
+args = sys.argv[1:]
+options = "hmo:f:"
+long_options = ["help", "my_file", "output=", "config="]
+default_ini = "sillypy.ini"
+Config = configparser.ConfigParser()
+Config.read(default_ini)
+try:
+    args, vals = getopt.getopt(args, options, long_options)
+    for arg, val in args:
+        if arg in ("-h", "--help"):
+            print(
+                "Usage: %s"
+                % """
+            Simple python script: takes arguments, prints out .ini file.
+            -h, --help: this help
+            -o, --output= : prints out value
+            -f, --config=config.ini : use config.ini instead of default
+            -m : returns own file name
+"""
+            )
+        elif arg in ("-m", "--my_file"):
+            print("Displaying file_name:", sys.argv[0])
+        elif arg in ("-o", "--output"):
+            print(f"-o value is: {val}")
+        elif arg in ("-f", "--config"):
+            Config = configparser.ConfigParser()
+            Config.read(val)
+except getopt.error as err:
+    print(str(err))
+#
+print(Config.sections())
+for section in Config.sections():
+    for option in Config[section]:
+        print(option, Config.get(section, option))
